0f8c836355ede8701be30a9055c0500ff7dca261
Nathan Strand <Nathan.Strand4267@gmail.com>
2017-05-19 20:32:15 -0500

    created a base user class

---
+++ model/user.php
@@ -0,0 +1,44 @@
+<?php
+    class User
+    {
+        private $username;
+        private $email;
+        private $bio;
+        private $portrait;
+        private $blogs[];
+        
+        function __construct($username="unknown", $email="unknown", $bio="unknown",
+                             $portrait="unknown", $blogs[])
+        {
+            $this->username = $username;
+            $this->email = $email;
+            $this->bio = $bio;
+            $this->portrait = $portrait;
+            $this->blogs = array();
+            
+        }
+        
+        function getUsername()
+        {
+            return $username;
+        }
+        
+        function getBio()
+        {
+            return $bio;
+        }
+        
+        function getPortrait()
+        {
+            return $portrait;
+        }
+        
+        function getAllBlogs()
+        {
+            return $blogs;
+        }
+        
+        function addBlog($blog)
+        {
+          $this->blogs[] = new Blog($blog);
+        }
